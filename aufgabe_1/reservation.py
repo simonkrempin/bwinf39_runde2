4b8eeccc8b1308a3d06f1fad603eae13f682bbcb
PhoenixTheDeveloper <67335158+PhoenixTheDev@users.noreply.github.com>
2021-03-25 19:12:06 +0100

    Update reservation.py

--- aufgabe_1/reservation.py
+++ aufgabe_1/reservation.py
@@ -1,7 +1,19 @@
+import random
+
+
 class Reservation:
     def __init__(self, input_arr):
         self.begin = input_arr[0]
         self.ending = input_arr[1]
         self.length = input_arr[2]
+        self.size = self.ending - self.begin
+
         self.x = -1
         self.y = -1
+
+        self.height_multiplier = 10
+
+        self.bottom_right = (self.length, self.size * self.height_multiplier)
+        self.top_left = (self.x, self.begin)
+
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
