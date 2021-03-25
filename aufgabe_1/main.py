6f552bc6f671c02c34e9098c8facf515560914dd
PhoenixTheDeveloper <67335158+PhoenixTheDev@users.noreply.github.com>
2021-03-25 20:21:37 +0100

    Update main.py

--- aufgabe_1/main.py
+++ aufgabe_1/main.py
@@ -6,7 +6,7 @@ import data_optimization as do
 time_range = 8, 18  # starting time and ending time of
 available_space = 1000
 
-file = open('example2.txt')
+file = open('example6.txt')
 reservations = []
 for i in range(int(file.readline())):
     reservations.append(rsv.Reservation(list(map(int, file.readline().split()))))
@@ -16,8 +16,9 @@ data_vis = dv.DataVisualization()
 data_vis.setup(reservations, 'before optimization', True)
 
 # data after optimization
+print('---- optimization -----')
 start_time = time.time()
 do = do.DataOptimization(reservations, time_range)
 optimized_reservations = do.optimize()
-print("--- %s seconds ---" % round((time.time() - start_time), 3))
+print("---- %s seconds ----" % round((time.time() - start_time), 3))
 data_vis.setup(reservations, 'after optimization', False)
